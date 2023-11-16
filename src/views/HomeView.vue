7a22ded40e94b75b1d8f9d58fac05a5526e01f08
christopher <christopher.pher39@gmail.com>
2023-11-16 00:02:30 -0600

    changes in buttons - home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -44,7 +44,7 @@ import ModalVue from '../components/Modal.vue';
             <span class="block text-[42px] md:text-[65px] font-bold">I'm </span>
             <img class="mt-1" src="https://readme-typing-svg.demolab.com?font=Roboto&weight=700&size=60&pause=1000&color=0DB1E3&vCenter=true&random=false&width=550&height=60&lines=Christoper;Frontend+developer;Designer;Backend+developer" alt="about image">
             <p class="mt-3 font-medium">I'm a passionate web development, Frontend | Backend.</p>
-           <div class="mt-5 flex items-center gap-5">
+           <div class="mt-5 flex items-center gap-3 md:gap-5">
             <RouterLink :to="{name: 'contact'}">
                 <button class="inline-flex items-center justify-center gap-2 px-4 py-2 rounded-full bg-pink-500 bg-opacity-30  hover:bg-opacity-40 transition-all duration-500">
                     <svg xmlns="http://www.w3.org/2000/svg" class="fill-current w-4 h-auto" viewBox="0 0 24 24"><path d="M20 2H4c-1.103 0-2 .894-2 1.992v12.017C2 17.106 2.897 18 4 18h3v4l6.351-4H20c1.103 0 2-.894 2-1.992V3.992A1.998 1.998 0 0 0 20 2zm-9 8a2 2 0 1 1-2-2c.086 0 .167.015.25.025.082-.014.164-.025.25-.025A1.5 1.5 0 0 1 11 9.5c0 .086-.012.168-.025.25.01.083.025.165.025.25zm4 2a2 2 0 0 1-2-2c0-.086.015-.167.025-.25A1.592 1.592 0 0 1 13 9.5 1.5 1.5 0 0 1 14.5 8c.086 0 .168.011.25.025.083-.01.164-.025.25-.025a2 2 0 0 1 0 4z"></path></svg>
