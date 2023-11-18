096f639e262a864fc8b069f42b776cf9703c50df
christopher <christopher.pher39@gmail.com>
2023-11-17 18:45:03 -0600

    changes in about image

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -43,7 +43,7 @@ import ModalVue from '../components/Modal.vue';
             <h2 class="font-semibold text-xs md:text-sm">Hello everyone!</h2>
             <span class="block text-[39px] md:text-[65px] font-bold">I'm </span>
             <img class="hidden md:block mt-1 z-20" src="https://readme-typing-svg.demolab.com?font=Roboto&weight=700&size=60&pause=1000&color=0DB1E3&vCenter=true&random=false&width=550&height=60&lines=Christoper;Frontend+developer;Designer;Backend+developer" alt="about image">
-            <img class="md:hidden z-20 -mt-1" src="https://readme-typing-svg.demolab.com?font=Roboto&weight=700&size=40&pause=1000&vCenter=true&random=false&height=60&lines=Christopher+;Backend+developer;Designer+;Frontend+developer" alt="">
+            <img class="md:hidden z-20 -mt-[1px]" src="https://readme-typing-svg.demolab.com?font=Roboto&weight=700&size=40&pause=1000&vCenter=true&random=false&height=60&lines=Christopher+;Backend+developer;Designer+;Frontend+developer" alt="">
             <p class="mt-3 font-medium">I'm a passionate web development, Frontend | Backend.</p>
            <div class="mt-5 flex items-center gap-3 md:gap-5">
             <RouterLink :to="{name: 'contact'}">
